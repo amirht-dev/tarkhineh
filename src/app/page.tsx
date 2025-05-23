321f9da53a2dcc3bf564bde32c1db774b99b40b7
Amir Hossein <amirht.dev@gmail.com>
2025-05-23 23:13:30 +0330

    add drop shadow style configs to tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div className="bg-primary-tint-1">خانه</div>;
+  return (
+    <div className="flex justify-around">
+      <div className="size-20 bg-primary drop-shadow-2"></div>
+      <div className="size-20 bg-primary drop-shadow-4"></div>
+      <div className="size-20 bg-primary drop-shadow-6"></div>
+      <div className="size-20 bg-primary drop-shadow-8"></div>
+      <div className="size-20 bg-primary drop-shadow-12"></div>
+      <div className="size-20 bg-primary drop-shadow-16"></div>
+    </div>
+  );
 }
