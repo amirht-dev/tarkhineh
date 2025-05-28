107051eb3b5aa52b4441ce5cbcfadb99d222115d
Amir Hossein <amirht.dev@gmail.com>
2025-05-28 12:04:24 +0330

    add SearchBox to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import SearchBox from "@/components/atoms/SearchBox";
 import AboutSection from "@/components/organisms/AboutSection";
 import HeroSlider from "@/components/organisms/HeroSlider";
 import RestaurantMenuSection from "@/components/organisms/RestaurantMenuSection";
@@ -6,6 +7,9 @@ export default function Home() {
   return (
     <div className="mb-6 space-y-6 lg:mb-12 lg:space-y-12">
       <HeroSlider />
+      <section className="container lg:hidden">
+        <SearchBox />
+      </section>
       <RestaurantMenuSection />
       <AboutSection />
     </div>
