9161fd34c0e4b20ee727098e76d98e478a6d7b6d
Amir Hossein <amirht.dev@gmail.com>
2025-07-25 20:01:24 +0330

    add next js util types

---
+++ src/types/next.ts
@@ -0,0 +1,81 @@
+import { FC } from "react";
+import { Simplify, UnionToIntersection } from "type-fest";
+
+type PromiseAble<T> = T | Promise<T>;
+
+type SingleParam<P extends string = string> = P;
+type CatchAllParam<P extends string = string> = P[];
+type OptionalCatchAllParam<P extends string = string> = P[][];
+
+type ParamObjectType<
+  TParamKey extends string = string,
+  TParamValue = unknown,
+> = Record<TParamKey, TParamValue>;
+
+type ConvertAbleParams<P extends string = string> =
+  | SingleParam<P>
+  | CatchAllParam<P>
+  | OptionalCatchAllParam<P>;
+
+type ParamType<P extends string = string> =
+  | ConvertAbleParams<P>
+  | ParamObjectType<P, unknown>;
+
+type ParamObject<T extends ConvertAbleParams> =
+  T extends OptionalCatchAllParam<infer P>
+    ? ParamObjectType<P, string[] | undefined>
+    : T extends CatchAllParam<infer P>
+      ? ParamObjectType<P, string[]>
+      : T extends SingleParam<infer P>
+        ? ParamObjectType<P, string>
+        : never;
+
+export type NextParams<
+  T extends ParamType,
+  TParentParams extends ParamObjectType = never,
+> = Simplify<
+  UnionToIntersection<
+    | (T extends ParamObjectType
+        ? T
+        : T extends ConvertAbleParams
+          ? ParamObject<T>
+          : never)
+    | TParentParams
+  >
+>;
+
+type NextParamsObject<TParams = never> = [TParams] extends [ParamType]
+  ? {
+      params: Promise<NextParams<TParams>>;
+    }
+  : never;
+
+// components
+export type NextPageProps<TParams extends ParamType = never> =
+  NextParamsObject<TParams>;
+
+export type NextPage<TParams extends ParamType = never> = FC<
+  NextPageProps<TParams>
+>;
+// ==========================
+
+// functions
+export type GenericStaticParamsReturnType<TParams extends ParamType> =
+  PromiseAble<Array<NextParams<TParams>>>;
+
+export type GenericStaticParams<TParams extends ParamType> =
+  () => GenericStaticParamsReturnType<TParams>;
+
+export type ServerFunction<TParams extends unknown[], TReturn = void> = (
+  ...params: TParams
+) => Promise<TReturn>;
+
+export type FormServerAction<
+  TParams extends unknown[] = [],
+  TReturn = void,
+> = ServerFunction<[...TParams, formData: FormData], TReturn>;
+
+export type UseActionStateFunction<T = void> = FormServerAction<
+  [currentState: T],
+  T
+>;
