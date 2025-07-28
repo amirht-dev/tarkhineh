6812880048189bc87cd6362a22303404f821fcdb
Amir Hossein <amirht.dev@gmail.com>
2025-07-28 16:20:43 +0330

    create branch image slider and branch info section in branch page

--- src/app/(slider)/branch/[branch_slug]/page.tsx
+++ src/app/(slider)/branch/[branch_slug]/page.tsx
@@ -1,12 +1,15 @@
 import { ResponsiveButton } from "@/components/atoms/Button";
 import { Note_Outline } from "@/components/atoms/icons/Content-Edit/Note";
+import ImageSlider, { ImageSliderSlide } from "@/components/atoms/ImageSlider";
 import {
   Section,
   SectionBody,
   SectionHeader,
   SectionTitle,
 } from "@/components/atoms/Section";
+import BranchInfo from "@/components/molecules/BranchInfo";
 import ProductCardSlider from "@/components/molecules/ProductCardSlider";
+import { heroSlides } from "@/constants";
 import { NextPage } from "@/types/next";
 
 const BranchPage: NextPage<"branch_slug"> = async ({ params }) => {
@@ -59,6 +62,34 @@ const BranchPage: NextPage<"branch_slug"> = async ({ params }) => {
           مشاهده منوی کامل
         </ResponsiveButton>
       </section>
+
+      <Section>
+        <SectionHeader className="flex justify-center">
+          <SectionTitle>شعبه اکباتان</SectionTitle>
+        </SectionHeader>
+
+        <SectionBody>
+          <ImageSlider
+            sidesNavigation
+            prevButtonClassName="[&_svg]:size-6 lg:[&_svg]:size-12"
+            nextButtonClassName="[&_svg]:size-6 lg:[&_svg]:size-12"
+          >
+            {heroSlides.map((slide, idx) => (
+              <ImageSliderSlide
+                overlay={false}
+                key={idx}
+                src={slide.imageSrc}
+                alt={slide.title}
+                className="overlay-20"
+              />
+            ))}
+          </ImageSlider>
+
+          <div className="container -mt-[15px] lg:-mt-[30px]">
+            <BranchInfo />
+          </div>
+        </SectionBody>
+      </Section>
     </>
   );
 };
