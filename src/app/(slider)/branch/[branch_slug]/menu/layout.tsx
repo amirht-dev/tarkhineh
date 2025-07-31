0ef241364a90e7c53949526719ed2642839ac761
Amir Hossein <amirht.dev@gmail.com>
2025-07-31 14:42:14 +0330

    add menu category tab bar to branch menu layout

--- src/app/(slider)/branch/[branch_slug]/menu/layout.tsx
+++ src/app/(slider)/branch/[branch_slug]/menu/layout.tsx
@@ -1,3 +1,5 @@
+import { TabBar, TabBarList, TabBarItem } from "@/components/molecules/TabBar";
+import { menus } from "@/constants";
 import { NextLayoutProps } from "@/types/next";
 
 export default async function BranchMenuLayout({
@@ -6,5 +8,22 @@ export default async function BranchMenuLayout({
 }: NextLayoutProps<"branch_slug">) {
   const { branch_slug } = await params;
 
-  return children;
+  return (
+    <>
+      <TabBar>
+        <TabBarList>
+          {Object.values(menus).map((menu) => (
+            <TabBarItem
+              key={menu.slug}
+              href={`/branch/${branch_slug}/menu/${menu.slug}`}
+            >
+              {menu.label}
+            </TabBarItem>
+          ))}
+        </TabBarList>
+      </TabBar>
+
+      {children}
+    </>
+  );
 }
