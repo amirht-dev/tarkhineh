5873ac1ee6d2011d85bfa232cd61efa9c0bf5c1d
Amir Hossein <amirht.dev@gmail.com>
2025-07-31 22:14:47 +0330

    add branch menu cards section in menu branch page

--- src/app/(slider)/branch/[branch_slug]/menu/[category_slug]/page.tsx
+++ src/app/(slider)/branch/[branch_slug]/menu/[category_slug]/page.tsx
@@ -1,3 +1,12 @@
+import Button from "@/components/atoms/Button";
+import { ShoppingCard_Outline } from "@/components/atoms/icons/Shop/ShoppingCard";
+import {
+  Section,
+  SectionBody,
+  SectionHeader,
+  SectionTitle,
+} from "@/components/atoms/Section";
+import MenuCard from "@/components/molecules/MenuCard";
 import { NextPageProps } from "@/types/next";
 
 export default async function MenuCategory({
@@ -5,5 +14,74 @@ export default async function MenuCategory({
 }: NextPageProps<"branch_slug" | "category_slug">) {
   const { category_slug } = await params;
 
-  return <div>{category_slug}</div>;
+  return (
+    <div className="my-6 space-y-6 lg:my-12 lg:space-y-12">
+      <Section className="container">
+        <SectionHeader className="flex items-center justify-between">
+          <SectionTitle>غذاهای ایرانی</SectionTitle>
+
+          <>
+            <Button
+              variant="outline"
+              size="sm"
+              className="lg:hidden"
+              prefixIcon={<ShoppingCard_Outline />}
+            >
+              تکمیل خرید
+            </Button>
+            <Button
+              variant="outline"
+              size="md"
+              className="max-lg:hidden"
+              prefixIcon={<ShoppingCard_Outline />}
+            >
+              تکمیل خرید
+            </Button>
+          </>
+        </SectionHeader>
+
+        <SectionBody className="grid grid-cols-1 gap-3 lg:grid-cols-2 lg:gap-6">
+          {Array.from({ length: 5 }, (_, idx) => (
+            <MenuCard fullWidth key={idx} />
+          ))}
+        </SectionBody>
+      </Section>
+
+      <Section className="container">
+        <SectionHeader className="justify-start">
+          <SectionTitle>غذاهای غیر ایرانی</SectionTitle>
+        </SectionHeader>
+
+        <SectionBody className="grid grid-cols-1 gap-3 lg:grid-cols-2 lg:gap-6">
+          {Array.from({ length: 5 }, (_, idx) => (
+            <MenuCard fullWidth key={idx} />
+          ))}
+        </SectionBody>
+      </Section>
+
+      <Section className="container">
+        <SectionHeader className="justify-start">
+          <SectionTitle>پیتزاها</SectionTitle>
+        </SectionHeader>
+
+        <SectionBody className="grid grid-cols-1 gap-3 lg:grid-cols-2 lg:gap-6">
+          {Array.from({ length: 5 }, (_, idx) => (
+            <MenuCard fullWidth key={idx} />
+          ))}
+        </SectionBody>
+      </Section>
+
+      <Section className="container">
+        <SectionHeader className="justify-start">
+          <SectionTitle>ساندویچ‌ها</SectionTitle>
+        </SectionHeader>
+
+        <SectionBody className="grid grid-cols-1 gap-3 lg:grid-cols-2 lg:gap-6">
+          {Array.from({ length: 5 }, (_, idx) => (
+            <MenuCard fullWidth key={idx} />
+          ))}
+        </SectionBody>
+      </Section>
+    </div>
+  );
 }
