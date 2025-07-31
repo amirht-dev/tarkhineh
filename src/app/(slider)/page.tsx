a20f5277508ab0fb408d52b8359bb8b63af9615d
Amir Hossein <amirht.dev@gmail.com>
2025-07-31 22:00:39 +0330

    fix space between sections in home route

--- src/app/(slider)/page.tsx
+++ src/app/(slider)/page.tsx
@@ -5,13 +5,13 @@ import RestaurantMenuSection from "@/components/organisms/RestaurantMenuSection"
 
 export default function Home() {
   return (
-    <>
+    <div className="my-6 space-y-6 lg:my-12 lg:space-y-12">
       <section className="container lg:hidden">
         <SearchBox />
       </section>
       <RestaurantMenuSection />
       <AboutSection />
       <BranchesSection />
-    </>
+    </div>
   );
 }
