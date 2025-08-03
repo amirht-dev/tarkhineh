c3d87607d2f98d8c6326d1f048fc9c43c009a3b4
Amir Hossein <amirht.dev@gmail.com>
2025-08-03 11:58:17 +0330

    update Steps stories

--- src/components/molecules/Steps/index.stories.tsx
+++ src/components/molecules/Steps/index.stories.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Button from "@/components/atoms/Button";
 import { ShoppingCard_Outline } from "@/components/atoms/icons/Shop/ShoppingCard";
+import { addToArrayAt } from "@/utils";
+import { useArgs } from "@storybook/preview-api";
 import type { Meta, StoryObj } from "@storybook/react";
 import { ReactNode } from "react";
 import {
@@ -12,15 +15,18 @@ import {
 } from ".";
 import { StepsProps } from "./index.types";
 
-type Step = { label: string; icon: ReactNode };
+type Step = { label: string; icon: ReactNode; disabled: boolean };
 
 type Props = {
   steps: Array<Step>;
 } & Pick<StepsProps, "initialStepIndex">;
 
-const createStep = (): Step => ({
+const createStep = ({
+  disabled = false,
+}: Partial<Pick<Step, "disabled">> = {}): Step => ({
   label: "سبد خرید",
   icon: <ShoppingCard_Outline />,
+  disabled,
 });
 
 const meta = {
@@ -33,22 +39,7 @@ const meta = {
     StepsView,
   },
   args: {
-    steps: Array.from({ length: 3 }, createStep),
-  },
-  render({ steps, initialStepIndex }) {
-    return (
-      <>
-        <Steps stepsCount={steps.length} initialStepIndex={initialStepIndex}>
-          <StepsBar>
-            {steps.map((step, idx) => (
-              <StepsBarItem key={idx} index={idx} prefixIcon={step.icon}>
-                {step.label} {idx}
-              </StepsBarItem>
-            ))}
-          </StepsBar>
-        </Steps>
-      </>
-    );
+    steps: Array.from({ length: 3 }, () => createStep()),
   },
 } satisfies Meta<Props>;
 
@@ -57,13 +48,27 @@ export default meta;
 type Story<T = typeof meta> = StoryObj<T>;
 
 export const Default = {
-  render({ steps, initialStepIndex }) {
+  render({ initialStepIndex }) {
+    const [{ steps }, updateArgs] = useArgs<Props>();
+
+    const updateStep = (index: number, update: Partial<Step>) => {
+      updateArgs({
+        steps: steps.map((step, idx) =>
+          idx === index ? { ...step, ...update } : step,
+        ),
+      });
+    };
     return (
       <>
         <Steps stepsCount={steps.length} initialStepIndex={initialStepIndex}>
           <StepsBar>
             {steps.map((step, idx) => (
-              <StepsBarItem key={idx} index={idx} prefixIcon={step.icon}>
+              <StepsBarItem
+                key={idx}
+                index={idx}
+                prefixIcon={step.icon}
+                disabled={step.disabled}
+              >
                 {step.label} {idx}
               </StepsBarItem>
             ))}
@@ -76,18 +81,59 @@ export const Default = {
                 index={idx}
                 className="bg-neutral-gray-3 flex h-[300px] flex-col items-center justify-center gap-4"
               >
-                <span>
-                  content of {step.label} {idx} step
-                </span>
+                {step.disabled ? null : (
+                  <>
+                    <span>
+                      content of {step.label} {idx} step
+                    </span>
 
-                <StepsNextButton stepIndex={idx} asChild>
-                  <Button>next step</Button>
-                </StepsNextButton>
+                    <StepsNextButton
+                      stepIndex={idx}
+                      disabled={steps[idx + 1]?.disabled}
+                      asChild
+                    >
+                      <Button>next step</Button>
+                    </StepsNextButton>
+                  </>
+                )}
               </StepsView>
             ))}
           </div>
         </Steps>
+
+        <table className="me-auto mt-4" dir="ltr">
+          <thead>
+            <tr>
+              <th className="border px-2">step</th>
+              <th className="border px-2">disabled</th>
+            </tr>
+          </thead>
+          <tbody>
+            {steps.map((steps, idx) => (
+              <tr key={idx}>
+                <td className="border px-2">{idx + 1}</td>
+                <td className="border px-2">
+                  <input
+                    type="checkbox"
+                    checked={steps.disabled}
+                    onChange={(e) => {
+                      updateStep(idx, { disabled: e.target.checked });
+                    }}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <Button
+          onClick={() =>
+            updateArgs({ steps: addToArrayAt(steps, 0, createStep()) })
+          }
+        >
+          add step at first
+        </Button>
       </>
     );
   },
-} satisfies Story;
+} satisfies Story<Props>;
