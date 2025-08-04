339bc10695a13c155fb9e757ff1c25562b48d3d8
Amir Hossein <amirht.dev@gmail.com>
2025-08-04 21:58:28 +0330

    create ShoppingCart checkout step

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,6 +2,7 @@
 
 import emptyDesktopImage from "@/assets/images/vectors/empty-desktop.png";
 import { ResponsiveButton } from "@/components/atoms/Button";
+import { ChevronLeft_Outline } from "@/components/atoms/icons/Arrow/ChevronLeft";
 import { CheckSquare_Outline } from "@/components/atoms/icons/Essential/CheckSquare";
 import { Wallet2_Outline } from "@/components/atoms/icons/Money/Wallet2";
 import { ShoppingCard_Outline } from "@/components/atoms/icons/Shop/ShoppingCard";
@@ -12,16 +13,19 @@ import {
   SectionHeader,
   SectionTitle,
 } from "@/components/atoms/Section";
+import Factor from "@/components/molecules/Factor";
+import ShoppingCard from "@/components/molecules/ShoppingCard";
 import {
   Steps,
   StepsBar,
   StepsBarItem,
   StepsPrevButton,
   StepsView,
+  useStepViewContext,
 } from "@/components/molecules/Steps";
-import { Slot } from "@radix-ui/react-slot";
+import Responsive, { Visible } from "@/components/utils/Responsive";
 import Image from "next/image";
-import { ComponentProps, ReactNode } from "react";
+import { ComponentProps, PropsWithChildren, ReactNode } from "react";
 import { ValueOf } from "type-fest";
 
 const step = {
@@ -42,35 +46,71 @@ export default function CheckoutPage() {
           ))}
         </StepsBar>
 
-        <CartStepView />
+        <StepViewContent index={0} label={step.cart.label}>
+          <ShoppingCartView />
+        </StepViewContent>
+
+        <StepViewContent index={1} label={step.information.label}>
+          <InformationView />
+        </StepViewContent>
       </Steps>
     </div>
   );
 }
 
-function CartStepView() {
-  const stepIndex = 0;
-  return (
-    <StepViewContent index={stepIndex} label={step.cart.label}>
-      <div className="border-neutral-gray-4 relative flex h-[375px] flex-col items-center justify-center gap-4 rounded-lg border lg:h-[422px] lg:gap-8">
-        <Image
-          src={emptyDesktopImage}
-          alt="empty"
-          className="absolute w-[200px] lg:w-[325px]"
+function ShoppingCartView() {
+  const { goToNextSiblingStep } = useStepViewContext();
+  const cart = 10;
+
+  if (cart > 0)
+    return (
+      <div className="flex items-start gap-6">
+        <div className="border-neutral-gray-4 flex max-h-[554px] flex-1 flex-col gap-4 overflow-auto rounded-lg border p-6 max-lg:hidden">
+          {Array.from({ length: cart }, (_, idx) => (
+            <ShoppingCard fullWidth key={idx} />
+          ))}
+        </div>
+
+        <Responsive
+          component={Factor}
+          showList={{ initial: true, lg: false }}
+          buttonAction={
+            <ResponsiveButton
+              className="w-full"
+              size={{ initial: "sm", lg: "md" }}
+              suffixIcon={<ChevronLeft_Outline />}
+              onClick={goToNextSiblingStep}
+            >
+              تکمیل اطلاعات
+            </ResponsiveButton>
+          }
         />
-        <p className="text-caption-md text-neutral-gray-7 lg:text-body-xl mt-12">
-          شما در حال حاضر هیچ سفارشی ثبت نکرده‌اید!
-        </p>
-        <ResponsiveButton
-          variant="outline"
-          size={{ initial: "sm", lg: "md" }}
-          className="w-38 justify-center"
-        >
-          منوی رستوران
-        </ResponsiveButton>
       </div>
-    </StepViewContent>
+    );
+
+  return (
+    <div className="border-neutral-gray-4 relative flex h-[375px] flex-col items-center justify-center gap-4 rounded-lg border lg:h-[422px] lg:gap-8">
+      <Image
+        src={emptyDesktopImage}
+        alt="empty"
+        className="absolute w-[200px] lg:w-[325px]"
+      />
+      <p className="text-caption-md text-neutral-gray-7 lg:text-body-xl mt-12">
+        شما در حال حاضر هیچ سفارشی ثبت نکرده‌اید!
+      </p>
+      <ResponsiveButton
+        variant="outline"
+        size={{ initial: "sm", lg: "md" }}
+        className="w-38 justify-center"
+      >
+        منوی رستوران
+      </ResponsiveButton>
+    </div>
   );
+}
+
+function InformationView() {
+  return null;
 }
 
 function StepViewContent({
@@ -78,29 +118,33 @@ function StepViewContent({
   index,
   label,
   sectionActionButton,
-}: Pick<ComponentProps<typeof StepsView>, "children" | "index"> &
-  Pick<ValueOf<typeof step>, "label"> & {
-    sectionActionButton?: ReactNode;
-  }) {
+}: PropsWithChildren<
+  Pick<ComponentProps<typeof StepsView>, "index"> &
+    Pick<ValueOf<typeof step>, "label"> & {
+      sectionActionButton?: ReactNode;
+    }
+>) {
   return (
     <StepsView index={index} key={index}>
-      <Section key={index} className="lg:hidden">
-        <SectionHeader className="grid grid-cols-3">
-          <SectionActionIconButton className="justify-self-start" asChild>
-            <StepsPrevButton className="disabled:invisible" />
-          </SectionActionIconButton>
-          <SectionTitle className="justify-self-center">{label}</SectionTitle>
-          {!!sectionActionButton && (
-            <SectionActionIconButton className="justify-self-end" asChild>
-              {sectionActionButton}
+      <Visible on="initial">
+        <Section key={index}>
+          <SectionHeader className="grid grid-cols-3">
+            <SectionActionIconButton className="justify-self-start" asChild>
+              <StepsPrevButton className="disabled:invisible" />
             </SectionActionIconButton>
-          )}
-        </SectionHeader>
+            <SectionTitle className="justify-self-center">{label}</SectionTitle>
+            {!!sectionActionButton && (
+              <SectionActionIconButton className="justify-self-end" asChild>
+                {sectionActionButton}
+              </SectionActionIconButton>
+            )}
+          </SectionHeader>
 
-        <SectionBody className="mt-6">{children}</SectionBody>
-      </Section>
+          <SectionBody className="mt-6">{children}</SectionBody>
+        </Section>
+      </Visible>
 
-      <Slot className="max-lg:hidden">{children}</Slot>
+      <Visible on="lg">{children}</Visible>
     </StepsView>
   );
 }
